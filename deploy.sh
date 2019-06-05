7e912a5424b9ce19fa29841d625ce622ca3e3548
Iason Papanagiotou <49448691+c18papa@users.noreply.github.com>
2019-06-05 23:30:12 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-./pandoc-2.6/bin/pandoc https://spirosbax.com/cv -f html-native_divs -o cv.pdf --pdf-engine=xelatex
+./pandoc-2.6/bin/pandoc https://c18papa.github.io/markdown-cv-c18papa/ -f html-native_divs -o cv.pdf --pdf-engine=xelatex
