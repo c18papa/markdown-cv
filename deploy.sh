b0398676f489e04bfe57bc8661b4a49a7f5de0ee
Iason Papanagiotou <49448691+c18papa@users.noreply.github.com>
2019-06-06 16:12:48 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-./pandoc-2.6/bin/pandoc https://c18papa.github.io/markdown-cv-c18papa/ -f html-native_divs -o cv.pdf --pdf-engine=xelatex --variable mainfont="Palatino"
+./pandoc-2.6/bin/pandoc https://c18papa.github.io/markdown-cv-c18papa/ -f html-native_divs -o cv.pdf --pdf-engine=xelatex
